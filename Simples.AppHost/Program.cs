a16038fa4955ac382d87541e6e2b0f210eac434d
Matthew Green <1550939+matt4446@users.noreply.github.com>
2024-12-06 00:41:10 +0000

    add home assistant

--- Simples.AppHost/Program.cs
+++ Simples.AppHost/Program.cs
@@ -1,10 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+var cache = builder.AddRedis("cache")
+    .WithLifetime(ContainerLifetime.Persistent);
 
 //https://github.com/dotnet/docs-aspire/blob/main/docs/community-toolkit/ollama.md
-var ollama = builder.AddOllama("ollama").WithContainerRuntimeArgs("--gpus=all");
+var ollama = builder.AddOllama("ollama").WithContainerRuntimeArgs("--gpus=all").WithLifetime(ContainerLifetime.Persistent);
 var phi35 = ollama.AddModel("phi3.5");
+
+builder.AddContainer("homeassistant", "homeassistant/home-assistant")
+    .WithVolume("config", "/etc/homeassistant")
+    .WithLifetime(ContainerLifetime.Persistent);
 
 var apiService = builder
     .AddProject<Projects.Simples_ApiService>("apiservice");
